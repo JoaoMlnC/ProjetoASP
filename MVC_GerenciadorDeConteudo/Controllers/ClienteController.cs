6bc2cf1f0f5be9a687d3f35faee45192d0587609
Logon Aluno <logonrmlocal@fiap.com.br>
2022-09-14 09:58:49 -0300

    Adicionando o novo controller

---
+++ MVC_GerenciadorDeConteudo/Controllers/ClienteController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_GerenciadorDeConteudo.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return View();
+            return Content("Olá Mundo");
+        }
+    }
+}
--- MVC_GerenciadorDeConteudo/Controllers/HomeController.cs
+++ MVC_GerenciadorDeConteudo/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace MVC_GerenciadorDeConteudo.Controllers
             return View();
         }
 
+        public IActionResult Contato()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
