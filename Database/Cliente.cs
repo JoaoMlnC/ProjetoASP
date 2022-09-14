08f785056c7a97ef1690d1ff061d977e28caa26c
Logon Aluno <logonrmlocal@fiap.com.br>
2022-09-14 08:55:01 -0300

    Adicionandoa classe Database

--- Database/Cliente.cs
+++ Database/Cliente.cs
@@ -4,16 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Business
+namespace Database
 {
     public class Cliente
     {
         public string Nome { get; set; }
 
-        public List<Cliente> Clientes (string cliente)
+        public List<Cliente> Clientes(string cliente)
         {
             var clientes = new List<Cliente>();
-            clientes.Add (new Cliente { Nome = "Elias" });
+            clientes.Add(new Cliente { Nome = "Elias" });
             return clientes;
         }
     }
